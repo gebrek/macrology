aef6ccca47c9a55d74940dfad80e25980140f095
Jacob Sonnenberg <jacob@x201>
2016-02-05 00:55:04 -0600

    broken nodes?

---
+++ nodes2.lisp
@@ -0,0 +1,27 @@
+(in-package :macrology)
+
+(defvar *nodes* nil)
+
+(defun defnode (&rest args)
+  (push args *nodes*)
+  args)
+
+(defun compile-net (root)
+  (let ((node (assoc root *nodes*)))
+    (if (null node)
+	nil
+	(let ((conts (second node))
+	      (yes (third node))
+	      (no (fourth node)))
+	  (if yes
+	      (let ((yes-fn (compile-net yes))
+		    (no-fn (compile-net no)))
+		#'(lambda ()
+		    (format t "~A~%>> " conts)
+		    (funcall (if (eq (read)
+				     'yes)
+				 yes-fn
+				 no-fn))))
+	      #'(lambda ()
+		  conts))))))
+
