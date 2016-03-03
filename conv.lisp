54d4e27c85c168828c6268ca6687fcd8250a0756
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-03-02 19:21:01 -0600

    conv

--- conv.lisp
+++ conv.lisp
@@ -1,9 +1,20 @@
 (defparameter *cube*
   (let ((cube (make-array '(3 3 3)))
-	(two-powers 1/2))
+	(counter 0))
     (loop for i from 0 upto 2 do
 	 (loop for j from 0 upto 2 do
 	      (loop for k from 0 upto 2 do
 		   (setf (aref cube i j k)
-			 (setf two-powers (* two-powers 2))))))
+			 (incf counter)))))
     cube))
+
+(defparameter *square*
+  (let ((sq (make-array '(2 2)))
+	(counter 0))
+    (loop for i from 0 upto 1 do
+	 (loop for j from 0 upto 1 do
+	      (setf (aref sq i j)
+		    (incf counter))))
+    sq))
+
+(defmacro array-loop (array (&rest binds) ))
